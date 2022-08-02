f0a7648579fa43e5f3936bb4b3d7edf0e37f0c3d
Minh Le <vietminhle98@gmail.com>
2022-08-02 14:45:26 +1000

    reset to bare landing page

---
+++ packages/web/app/landing/landing.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+export const LandingPage: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>
+          Z-Ray | Search for anything across all of your apps and tools
+        </title>
+        <meta
+          name="description"
+          content="Search for anything across all of your apps and tools"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>hi</main>
+    </div>
+  );
+};
