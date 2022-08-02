3cc2439d88ae921455a41b5c06d2554c33c28c96
Minh Le <vietminhle98@gmail.com>
2022-08-02 16:01:36 +1000

    re position logo

--- packages/web/base/header/header.tsx
+++ packages/web/base/header/header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.brandBlock}>
-        <Image src="/logo.svg" width={80} height={80} />
+        <Image src="/logo.svg" width={32} height={32} />
         <h3 className={styles.brandTitle}>Z-Ray</h3>
       </div>
       <Button>Get beta access</Button>
