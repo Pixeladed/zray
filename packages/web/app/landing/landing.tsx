5fbf3539ad8c30db1638989eb4d4456913d6a154
Pixeladed <vietminhle98@gmail.com>
2022-08-03 20:11:38 +1000

    add layout

--- packages/web/app/landing/landing.tsx
+++ packages/web/app/landing/landing.tsx
@@ -24,14 +24,30 @@ export const LandingPage: NextPage = () => {
           <p className={styles.heroLead}>
             Find anything across all of your apps and tools
           </p>
-          <div className={styles.heroImage}>
-            <img
-              src="https://via.placeholder.com/800x400"
-              alt="Z-Ray product screenshot"
-              width={800}
-              height={400}
-            />
-          </div>
+          <img
+            src="https://via.placeholder.com/800x400"
+            alt="Z-Ray product screenshot"
+            width={800}
+            height={400}
+            className={styles.heroImage}
+          />
+        </section>
+        <section className={styles.section}>
+          <h2 className={styles.sectionTitle}>
+            See through everything, all at once
+          </h2>
+          <p className={styles.sectionLead}>
+            That&apos;s why we call it Z-Ray, giving you full visibility over
+            all your work, instantly.
+          </p>
+        </section>
+        <section className={styles.section}>
+          <h2 className={styles.sectionTitle}>Your data is your data</h2>
+          <p className={styles.sectionLead}>
+            Z-Ray runs everything locally on your device, your account
+            credentials are never sent to us and we don&apos;t store your data
+            on our servers.
+          </p>
         </section>
       </main>
     </div>
