822b74821173cdd44b6c73ae5329b20e80795d74
Minh Le <vietminhle98@gmail.com>
2022-08-08 14:37:46 +1000

    decouple integration from provider

--- packages/app/src/services/search/provider.ts
+++ packages/app/src/services/search/provider.ts
@@ -1,13 +1,21 @@
-export interface Provider extends ProviderInfo {
+/**
+ * A provider is an instance of an integration that is capable of taking in a query
+ * and return a list of search results for said query. e.g. xyz@email.com provider for the Email integration
+ */
+export interface Provider {
+  id: string;
   search(query: string, options: { page: number }): Promise<SearchResult[]>;
   authenticate(): Promise<OperationResult>;
-  connect(): Promise<OperationResult>;
 }
 
-export interface ProviderInfo {
-  id: string;
+/**
+ * An integration is a type of connector that can be configured to create a new provider
+ * an integration itself cannot provide searching capabilities
+ */
+export interface Integration {
   name: string;
   icon?: string;
+  connect(): Promise<OperationResult<Provider>>;
 }
 
 export type SearchResult = {
@@ -16,7 +24,11 @@ export type SearchResult = {
   url: string;
 };
 
-export type OperationResult = { success: boolean; cancelled: boolean };
+export type OperationResult<T = undefined> = {
+  data: T;
+  success: boolean;
+  cancelled: boolean;
+};
 
 export class UnauthenticatedProviderError extends Error {
   name = 'UnauthenticatedProviderError';
