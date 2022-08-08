7612e6cccfe2678d95d4c3737637a4b68bbde8c8
Minh Le <vietminhle98@gmail.com>
2022-08-08 10:29:00 +1000

    add search bar styling

--- packages/app/src/pages/search/search.tsx
+++ packages/app/src/pages/search/search.tsx
@@ -2,15 +2,8 @@ import styles from './search.module.css';
 
 export function SearchPage() {
   return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className={styles.page}>
+      <input placeholder="What are you looking for?" className={styles.input} />
     </div>
   );
 }
