30c293d387e32861bc3c53b7640eea8589f220d1
Minh Le <vietminhle98@gmail.com>
2022-08-23 13:13:46 +1000

    fix impl

--- packages/app/src/native/services/slack/slack_native_integration.ts
+++ packages/app/src/native/services/slack/slack_native_integration.ts
@@ -1,7 +1,7 @@
 import { Path } from '../../../base/path';
-import { NativeIntegration } from '../integration/integration_native_service';
+import { IntegrationInfo } from '../../../interface/intergration';
 
-export class SlackNativeIntegration implements NativeIntegration {
+export class SlackNativeIntegration implements IntegrationInfo {
   id = 'com.builtin.slack';
   name = 'Slack';
   icon = Path.resource('/integrations/slack/slack.svg');
