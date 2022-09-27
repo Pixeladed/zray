751d8048caa6ee6bf2d3104bed655264a9996614
Minh Le <vietminhle98@gmail.com>
2022-09-27 14:23:21 +1000

    disable auth gate

--- packages/app/src/base/auth/auth_gate.tsx
+++ packages/app/src/base/auth/auth_gate.tsx
@@ -6,24 +6,24 @@ export const AuthGate = ({
   children,
   LoginButton,
 }: React.PropsWithChildren<{ LoginButton: ComponentType }>) => {
-  const { isAuthenticated, isLoading, user } = useAuth0();
-  console.table({
-    isAuthenticated,
-    isLoading,
-  });
-  console.log('user', user);
+  // const { isAuthenticated, isLoading, user } = useAuth0();
+  // console.table({
+  //   isAuthenticated,
+  //   isLoading,
+  // });
+  // console.log('user', user);
 
-  if (isLoading) {
-    return <div className={styles.container}>Loading...</div>;
-  }
+  // if (isLoading) {
+  //   return <div className={styles.container}>Loading...</div>;
+  // }
 
-  if (!isAuthenticated) {
-    return (
-      <div className={styles.container}>
-        <LoginButton />
-      </div>
-    );
-  }
+  // if (!isAuthenticated) {
+  //   return (
+  //     <div className={styles.container}>
+  //       <LoginButton />
+  //     </div>
+  //   );
+  // }
 
   return <>{children}</>;
 };
