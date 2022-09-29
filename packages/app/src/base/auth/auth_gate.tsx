1968522d86f4e7ff6887b5b5388f68bced4a6a32
Minh Le <vietminhle98@gmail.com>
2022-09-29 13:35:49 +1000

    remove auth0 libs

--- packages/app/src/base/auth/auth_gate.tsx
+++ packages/app/src/base/auth/auth_gate.tsx
@@ -1,29 +1,6 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import React, { ComponentType } from 'react';
+import React from 'react';
 import styles from './auth_gate.module.css';
 
-export const AuthGate = ({
-  children,
-  LoginButton,
-}: React.PropsWithChildren<{ LoginButton: ComponentType }>) => {
-  // const { isAuthenticated, isLoading, user } = useAuth0();
-  // console.table({
-  //   isAuthenticated,
-  //   isLoading,
-  // });
-  // console.log('user', user);
-
-  // if (isLoading) {
-  //   return <div className={styles.container}>Loading...</div>;
-  // }
-
-  // if (!isAuthenticated) {
-  //   return (
-  //     <div className={styles.container}>
-  //       <LoginButton />
-  //     </div>
-  //   );
-  // }
-
+export const AuthGate = ({ children }: React.PropsWithChildren) => {
   return <>{children}</>;
 };
--- packages/app/src/base/auth/create.tsx
+++ packages/app/src/base/auth/create.tsx
@@ -1,9 +1,6 @@
-import { Auth0Provider } from '@auth0/auth0-react';
 import { Auth0Config } from '../../web/base/config';
 import { Routes } from '../../routes';
-import { Auth0Client } from '@auth0/auth0-spa-js';
 import { AuthGate } from './auth_gate';
-import { Button } from '@highbeam/components';
 
 export const createAuth = ({
   config,
@@ -14,48 +11,12 @@ export const createAuth = ({
 }) => {
   const redirectUrl = new URL(redirectOrigin);
   redirectUrl.pathname = Routes.loginCallback().absolute;
-  const authClient = new Auth0Client({
-    client_id: config.clientId,
-    domain: config.domain,
-    audience: config.audience,
-    redirect_uri: redirectUrl.toString(),
-  });
-
-  authClient.checkSession();
-
-  const AuthProvider = ({ children }: React.PropsWithChildren) => (
-    <Auth0Provider
-      clientId={config.clientId}
-      domain={config.domain}
-      audience={config.audience}
-      redirectUri={redirectUrl.toString()}
-      children={children}
-      cacheLocation="localstorage"
-      useRefreshTokens={true}
-    />
-  );
-
-  const login = () => {
-    authClient.loginWithRedirect();
-  };
-  const LoginButton = () => <Button onClick={login}>Login</Button>;
 
   const AuthGateImpl = ({ children }: React.PropsWithChildren) => (
-    <AuthGate LoginButton={LoginButton}>{children}</AuthGate>
+    <AuthGate>{children}</AuthGate>
   );
 
-  const handleCallback = async (url: string) => {
-    const creds = await authClient.handleRedirectCallback(url);
-    console.log('authenticated', creds);
-    console.log('token?', await authClient.getUser());
-    console.log('token?', await authClient.getTokenSilently());
-    window.location.reload();
-  };
-
   return {
-    AuthProvider,
-    authClient,
     AuthGate: AuthGateImpl,
-    handleCallback,
   };
 };
