daf2d6929c3acad84375f4e1762c14ed19c94bdd
Minh Le <vietminhle98@gmail.com>
2022-10-05 16:52:46 +1100

    add billing skeleton

--- packages/app/src/web/pages/settings/account/account.tsx
+++ packages/app/src/web/pages/settings/account/account.tsx
@@ -1,9 +1,20 @@
 import { Button } from '@highbeam/components';
+import { ExternalLink } from 'react-feather';
 import styles from './account.module.css';
 
 export const AccountSettings = ({ onLogout }: { onLogout: () => void }) => {
   return (
     <div className={styles.container}>
+      <h3 className={styles.heading}>Billing</h3>
+      <p>
+        Highbeam partners with Stripe to manage billing and payments. You can
+        use the billing portal to manage your payment method and switch plans.
+      </p>
+      <Button>
+        Billing portal&nbsp;&nbsp;
+        <ExternalLink size="1em" />
+      </Button>
+      <hr className={styles.divider} />
       <Button variant="danger" onClick={onLogout}>
         Logout
       </Button>
