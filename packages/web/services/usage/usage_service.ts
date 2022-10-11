869d88f943f2a8ac6103ba4a794260d356da3c5a
Minh Le <vietminhle98@gmail.com>
2022-10-11 11:10:30 +1100

    return integration limit and plan name

--- packages/web/services/usage/usage_service.ts
+++ packages/web/services/usage/usage_service.ts
@@ -3,6 +3,9 @@ import { Usage } from '@highbeam/interface';
 import { Stripe } from 'stripe';
 import { AuthService } from '../auth/auth_service';
 
+const STRIPE_METADATA_INTEGRATION_LIMIT_FIELD = 'integration_limit';
+const STRIPE_METADATA_INTEGRATION_LIMIT_UNLIMITED = -1;
+
 export class UsageService {
   stripe: Stripe;
 
@@ -19,12 +22,43 @@ export class UsageService {
   ) => {
     const userId = await this.authService.userIdFromHeader(req.headers);
     const customer = await this.getOrCreateStripeCustomer(userId);
-    return res.json({ plan: 'free' });
+
+    if (!customer.subscriptions) {
+      throw new Error('Customer does not have any subscription');
+    }
+
+    const products = customer.subscriptions.data.flatMap(subscription =>
+      subscription.items.data.map(item => item.plan.product as Stripe.Product)
+    );
+
+    if (products.length > 1) {
+      throw new Error('Customer is subscribed to multiple products');
+    }
+
+    const product = products[0];
+    const recordedLimit = parseInt(
+      product.metadata[STRIPE_METADATA_INTEGRATION_LIMIT_FIELD]
+    );
+
+    if (Number.isNaN(recordedLimit)) {
+      throw new Error('Product integration limit is not a number');
+    }
+
+    const integrationLimit =
+      recordedLimit === STRIPE_METADATA_INTEGRATION_LIMIT_UNLIMITED
+        ? null
+        : recordedLimit;
+
+    return res.json({
+      name: product.name,
+      integrationLimit,
+    });
   };
 
   private getOrCreateStripeCustomer = async (userId: string) => {
     const existing = await this.stripe.customers.search({
       query: `metadata["auth0Id"]:"${userId}"`,
+      expand: ['subscriptions.items.plan.product'],
     });
 
     if (existing.data.length > 1) {
