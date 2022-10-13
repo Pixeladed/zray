3b35e6c2e9c6d4e0c9bb0115301ec06cc189fca7
Minh Le <vietminhle98@gmail.com>
2022-10-13 15:53:03 +1100

    add header and hero

--- packages/web/app/landing/landing.tsx
+++ packages/web/app/landing/landing.tsx
@@ -2,6 +2,7 @@ import { Button } from '@highbeam/components';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './landing.module.css';
 
 export const LandingPage: NextPage = () => {
@@ -18,18 +19,36 @@ export const LandingPage: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <header className={styles.header}>
+        <div className={styles.brandBlock}>
+          <Image src="/icon.png" width={32} height={32} />
+          <h1 className={styles.headerTitle}>Highbeam</h1>
+        </div>
+        <div className={styles.menu}>
+          <Link passHref={true} href="/product">
+            <a>Product</a>
+          </Link>
+          <Link passHref={true} href="/integrations">
+            <a>Integrations</a>
+          </Link>
+          <Link passHref={true} href="/pricing">
+            <a>Pricing</a>
+          </Link>
+        </div>
+        <Button variant="primary">Download for Mac</Button>
+      </header>
+      <main className={styles.content}>
         <section className={styles.hero}>
-          <Image src="/icon.png" width={96} height={96} />
-          <h1 className={styles.heroTitle}>
-            Find anything, anywhere, all at once
-          </h1>
-          <p className={styles.heroLead}>
-            Search for any files, messages, tasks or events
+          <h2 className={styles.heroTitle}>
+            Find anything
+            <br />
+            with one search
+          </h2>
+          <p className={styles.lead}>
+            Connect your favorite apps and use Highbeam
+            <br /> to look for anything with one single search
           </p>
-          <Button variant="primary">
-            <span className={styles.heroCta}>Download Highbeam for Mac</span>
-          </Button>
+          <Button variant="primary">Download for Mac</Button>
         </section>
       </main>
     </div>
