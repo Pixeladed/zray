09f8697e94b88d7979fdfd0698b78df5268e077b
Minh Le <vietminhle98@gmail.com>
2022-10-20 10:38:05 +1100

    fix gcal scope

--- packages/web/services/google_calendar/create.ts
+++ packages/web/services/google_calendar/create.ts
@@ -2,9 +2,8 @@ import { GoogleOAuthConfig } from '../../base/config';
 import { GoogleService } from '../google/google_service';
 
 export const createGoogleCalendarService = (config: GoogleOAuthConfig) => {
-  const scopes = ['https://www.googleapis.com/auth/calendar.events.readonly	'];
+  const scopes = ['https://www.googleapis.com/auth/calendar.events.readonly'];
   const googleCalendarService = new GoogleService(config, scopes);
-  console.log('calendar config with scope', config);
 
   return { googleCalendarService };
 };
