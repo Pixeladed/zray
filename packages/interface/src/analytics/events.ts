02a57e716823441b435e8ae58a670d9867b0e00b
Minh Le <vietminhle98@gmail.com>
2022-10-20 11:41:54 +1100

    add search events

--- packages/interface/src/analytics/events.ts
+++ packages/interface/src/analytics/events.ts
@@ -3,3 +3,22 @@ export type EventName<T> = T extends Event<infer N, any> ? N : never;
 export type EventProp<T> = T extends Event<any, infer P> ? P : never;
 
 // event definitions
+export type SearchStartedEvent = Event<
+  'search_started',
+  { searchId: string; profileCount: number; integrationCount: number }
+>;
+
+export type SearchCompletedEvent = Event<
+  'search_completed',
+  { searchId: string; resultsCount: number }
+>;
+
+export type SearchResultSeen = Event<
+  'search_result_seen',
+  { searchId: string; integrationId: string }
+>;
+
+export type SearchResultClickedEvent = Event<
+  'search_result_clicked',
+  { searchId: string; integrationId: string }
+>;
