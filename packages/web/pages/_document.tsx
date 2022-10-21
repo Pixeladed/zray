799b66a57a4886b02a2f38868f955623d8e51e04
Minh Le <vietminhle98@gmail.com>
2022-10-21 15:17:14 +1100

    add GA measuring script

--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -1,6 +1,7 @@
 // pages/_document.js
 
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -15,6 +16,19 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-3S4LVQZX5S"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-3S4LVQZX5S');
+            `}
+          </Script>
         </body>
       </Html>
     );
